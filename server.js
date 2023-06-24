e906d76e6b6b0968943033575ecad42c2e9119f3
Jlevbury <125767916+Jlevbury@users.noreply.github.com>
2023-06-24 16:36:36 -0400

    initial push of dependencies

---
+++ server.js
@@ -0,0 +1,20 @@
+// Import dependencies
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+// Create an Express app
+const app = express();
+
+// Configure Handlebars as the template engine
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Set the path to your views directory (where your Handlebars templates will reside)
+app.set('views', path.join(__dirname, 'views'));
+
+// Set up your routes and other middleware here
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on http://localhost:3000');
+});
