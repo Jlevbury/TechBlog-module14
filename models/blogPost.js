a52352a265b8660c59541b77cb1d7f7ef069c23e
Jlevbury <125767916+Jlevbury@users.noreply.github.com>
2023-06-29 11:30:59 -0400

    pushing current status to troubleshoot with ASKBCS

--- models/blogPost.js
+++ models/blogPost.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('./index');
+const sequelize = require('../config/connection');
+
 
 const BlogPost = sequelize.define('BlogPost', {
   title: {
--- server.js
+++ server.js
@@ -1,36 +1,3 @@
-/*const express = require('express');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const path = require('path');
-
-// Import your routes here
-const authRoutes = require('./routes/index');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Set up Handlebars as the template engine
-//app.engine('handlebars', exphbs());
-
-app.set('view engine', 'handlebars');
-
-// Set up the static directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Set up session middleware
-app.use(session({
-  secret: 'your_secret_key',
-  resave: false,
-  saveUninitialized: false,
-}));
-
-// Set up your routes here
-app.use('/auth', authRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
-});
-*/
 const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
@@ -72,7 +39,7 @@ app.use(
 );
 
 // Set up your routes here
-app.use('/routes/api/index.js', authRoutes);
+app.use('/auth', authRoutes);
 
 // Homepage route
 app.get('/', async (req, res) => {
